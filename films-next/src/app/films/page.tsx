180515f7ac710ff1595ee8c15dfadce3f4bfa322
faun3 <antonioculcea@gmail.com>
2023-08-30 16:38:17 +0300

    start work on page component

---
+++ films-next/src/app/films/page.tsx
@@ -0,0 +1,11 @@
+// server component
+
+// IMPORTANT
+// to use this, set up a .env file with the following public nextjs secret
+// NEXT_PUBLIC_STRAPI_API="http://locahost:1337/api" - for a locally hosted instance of strapi
+// IMPORTANT
+
+const Page = async () => {
+  return <div></div>;
+};
+export default Page;
