949e13bca9abc2ce807bf6e9d3119bfb78b9d149
faun3 <antonioculcea@gmail.com>
2023-08-30 15:09:39 +0300

    add a redirecting hover link to the nav

---
+++ films-next/src/app/testPage/page.tsx
@@ -0,0 +1,8 @@
+const Page = () => {
+  return (
+    <div className="my-10">
+      <h1>Testing testing</h1>
+    </div>
+  );
+};
+export default Page;
--- films-next/src/components/Nav.tsx
+++ films-next/src/components/Nav.tsx
@@ -9,20 +9,30 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
   NavigationMenuViewport,
+  navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
+import Link from "next/link";
 
 const Nav = () => {
   return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink>Link</NavigationMenuLink>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+    <div className="w-full flex justify-between max-w-[1248px] mx-auto">
+      <span>Test</span>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem></NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <Link href="/testPage">
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Link
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
   );
 };
 export default Nav;
